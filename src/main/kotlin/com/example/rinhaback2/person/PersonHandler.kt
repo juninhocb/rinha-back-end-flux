ffd4c4769828695da56b8b985bc8f3e1251ee166
juninhocb <juninhocb@hotmail.com>
2023-09-17 03:30:36 -0300

    final

--- src/main/kotlin/com/example/rinhaback2/person/PersonHandler.kt
+++ src/main/kotlin/com/example/rinhaback2/person/PersonHandler.kt
@@ -6,26 +6,26 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Mono
-import java.util.*
 
 @Component
 class PersonHandler(private val repository: PersonRepository,
                     private val redisTemplate: RedisTemplate<String, Person>) {
 
-    fun create(request: ServerRequest) : Mono<ServerResponse> {
+    fun create(request: ServerRequest): Mono<ServerResponse> {
         return request.bodyToMono(Person::class.java).flatMap {
 
             if (it.nome.length > 100
                 || it.apelido.length > 32
-                || !it.nascimento.matches("\\d{4}-\\d{2}-\\d{2}".toRegex())) {
-                ServerResponse.unprocessableEntity().build()
+                || !it.nascimento.matches("\\d{4}-\\d{2}-\\d{2}".toRegex())
+            ) {
+                return@flatMap ServerResponse.unprocessableEntity().build()
             }
 
 
             val addedNickname = redisTemplate.opsForValue().get(it.apelido)
 
-            if (addedNickname != null){
-                ServerResponse.unprocessableEntity().build()
+            if (addedNickname != null) {
+                return@flatMap ServerResponse.unprocessableEntity().build()
             }
 
 
@@ -35,7 +35,7 @@ class PersonHandler(private val repository: PersonRepository,
                 id = id,
                 nome = it.nome,
                 apelido = it.apelido,
-                nascimento =  it.nascimento,
+                nascimento = it.nascimento,
                 stack = it.stack
             )
 
@@ -47,6 +47,7 @@ class PersonHandler(private val repository: PersonRepository,
         }
 
     }
+
     fun findById(request: ServerRequest): Mono<ServerResponse> {
         val id = request.pathVariable("id")
 
@@ -55,19 +56,22 @@ class PersonHandler(private val repository: PersonRepository,
         return if (cachedPerson != null) {
             ServerResponse.ok().bodyValue(cachedPerson)
         } else {
-            ServerResponse.ok().body(repository.findById(UUID.fromString(id)), Person::class.java)
+            //ServerResponse.ok().body(repository.findById(UUID.fromString(id)), Person::class.java)
+            ServerResponse.badRequest().build()
         }
     }
 
-    fun findByCriteria(request: ServerRequest) : Mono<ServerResponse> {
+    fun findByCriteria(request: ServerRequest): Mono<ServerResponse> {
 
         val criteria = request.queryParam("t").orElse("")
 
         if (criteria.isBlank()) {
             return ServerResponse.badRequest().build()
         }
-        return ServerResponse.ok().body(Mono.just(repository.findByCriteria(criteria)) , List::class.java)
+        return ServerResponse.ok().body(Mono.just(repository.findByCriteria(criteria)), List::class.java)
     }
-    fun count(request: ServerRequest?) : Mono<ServerResponse>{
+
+    fun count(request: ServerRequest?): Mono<ServerResponse> {
         return ServerResponse.ok().body(Mono.just(repository.count()), Long::class.java)
     }
+}
